374f26f76dfb67971eecaa910a3756bf213676a3
Kien Ngo <kenseverus@gmail.com>
2022-08-01 07:14:22 -0500

    Update TaskStatusBar

--- src/components/tasks/TaskStatusBar.jsx
+++ src/components/tasks/TaskStatusBar.jsx
@@ -1,14 +1,45 @@
 import { FaList, FaCalendarCheck, FaCalendarDay } from "react-icons/fa";
 
-export default function TaskStatusBar(props) {
-  const iconSpacing = {
-    marginRight: '.5rem',
-  };
+const iconSpacing = {
+  marginRight: ".5rem",
+};
 
+const StatusItems = [
+  {
+    label: "To Do",
+    value: false,
+    component: <FaCalendarDay style={iconSpacing} />,
+  },
+  {
+    label: "Done",
+    value: true,
+    component: <FaCalendarCheck style={iconSpacing} />,
+  },
+  {
+    label: "All",
+    value: null,
+    component: <FaList style={iconSpacing} />,
+  },
+];
+
+export default function TaskStatusBar(props) {
   return (
     <div className="tabs is-fullwidth is-boxed is-medium">
       <ul className="task-status">
-        <li className={`task-status-item ${props.status === false ? 'is-active' : ''}`} data-status={false}>
+        {StatusItems.map((item) => (
+          <li
+            key={item.label}
+            className={`task-status-item ${props.status === item.value ? "is-active" : ""}`}
+            data-status={item.value}
+          >
+            <a className="has-text-weight-bold">
+              {item.component}
+              <span className="is-hidden-touch">{item.label}</span>
+            </a>
+          </li>
+        ))}
+        {/* Old Code */}
+        {/* <li className={`task-status-item ${props.status === false ? 'is-active' : ''}`} data-status={false}>
           <a className="has-text-weight-bold">
             <FaCalendarDay style={iconSpacing} />
             <span className="is-hidden-touch">To Do</span>
@@ -25,7 +56,7 @@ export default function TaskStatusBar(props) {
             <FaList style={iconSpacing} />
             <span className="is-hidden-touch">All</span>
           </a>
-        </li>
+        </li> */}
       </ul>
     </div>
   );
